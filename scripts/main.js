8644267aba49fc5807ee92d66567667d390d47ec
ivanepais <55938095+ivanepais@users.noreply.github.com>
2021-09-08 22:29:26 -0300

    Add files via upload
    Practicas del curso

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/*let saludo = 'hola mundo';
+alert(saludo);*/
+
+//Reinicio de la reproduccion multimedia: seleccion mejor fuente y restablecer al principio
+//var mediaElem = document.getElementById("my-media-element");
+//mediaElem.load(); Ver documentacion sobre la funcion load()
+
+//Detectar la adicion y eliminacion de pistas: ver la lista de pista dentro de un elemento multimedia
+//var mediaElem = document.querySelector("video");
+//mediaElem.audioTracks.onaddtrack = function(event) {
+//  audioTrackAdded(event.track); Ver documentacion audiotracklist y trackevent
+//}
+
+
